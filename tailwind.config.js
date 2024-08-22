19e7214eea77c8e5f303d1928d25c8cac9376bc6
anaybhadury <bhadury9292@gmail.com>
2024-08-22 20:49:20 +0600

    added navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-            clifford: '#da373d',
-            'primary-color':'',
+            'primary-color':'#ABEF5F',
           },
             backgroundImage:{
             'hero-bg': "url('')",
